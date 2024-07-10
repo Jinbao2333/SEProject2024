d8845ec7c8dfcdd725636cd5952c9285beabb866
林童奕凡 <129384364+Floyed11@users.noreply.github.com>
2024-07-10 14:13:26 +0800

    Update sketch_jun15a.ino loop

--- Arduino/sketch_jun15a.ino
+++ Arduino/sketch_jun15a.ino
@@ -31,3 +31,75 @@ void setup() {
   }
   // Serial.println("Waiting for a card......");
 }
+
+void loop() {
+
+  uint8_t dataWrite[4] = {2, 0, 2, 3};
+  uint8_t dataRead[4];
+
+  if (nfc.scan()) {
+    /*!
+      The NTAG's EEPROM memory is organized in pages with 4 bytes per page. NTAG213 variant has
+      45 pages, NTAG215 variant has 135 pages and NTAG216 variant has 231 pages in total.
+      The memory organization can be seen  in the following table .
+
+           page Adr      BYTE number within a page
+                         0            |    1    |   2       |    3      |
+              0                       serial  number
+              1                       serial  number
+              2          serial number|internal | lock bytes| lock bytes|
+              3                       Capability Container(cc)
+              ----------------------------------------------------------------
+              -----------------------NTAG213----------------------------------
+              4
+              ...                     user memory
+              39
+              ----------------------------------------------------------------
+              ------------------------NTAG215---------------------------------
+              4
+              ...                     user memory
+              129
+              ----------------------------------------------------------------
+              ------------------------NTAG216---------------------------------
+              4
+              ...                     user memory
+              225
+    */  NFCcard = nfc.getInformation();
+    if (NFCcard.AQTA[1] == 0x44 && memcmp(NFCcard.cardType, "Ultralight", 10) != 0) {
+      // Serial.print("Data to be written(HEX):");
+      /* for (int i = 0; i < 4; i++) {
+        Serial.print(dataWrite[i], HEX);
+        Serial.print(" ");
+      } */
+      Serial.println("");
+      // Serial.print("The execution result:");
+      if (nfc.writeNTAG(block, dataWrite) == 1) {
+        // Serial.println("write success");
+      }
+      else {
+        Serial.println("write failure");
+      }
+      // Serial.print("Data read(HEX):");
+      if (nfc.readNTAG(dataRead, block) == 1) {
+
+        Serial.print(":");
+        for (int i = 0; i < 4; i++) {
+          Serial.print(dataRead[i], HEX);
+          // Serial.print(" ");
+        }
+        Serial.print("#");
+        // Serial.println("");
+      }
+      else {
+        Serial.println("Read failure");
+      }
+    }
+    else {
+      Serial.println("The card type is not NTAG21x...");
+    }
+  }
+  else {
+    //Serial.println("no card");
+  }
+  delay(2000);
+}
