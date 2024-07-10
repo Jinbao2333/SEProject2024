e3ae00db450bbfdfbeaf69f31f773729d8e8124f
林童奕凡 <129384364+Floyed11@users.noreply.github.com>
2024-07-10 14:02:11 +0800

    Update sketch_jun15a.ino setup

--- Arduino/sketch_jun15a.ino
+++ Arduino/sketch_jun15a.ino
@@ -10,3 +10,24 @@
  *@date  2019-7-3
  *@url https://github.com/DFRobot/DFRobot_PN532
  */
+#include <DFRobot_PN532.h>
+
+#define block          (5)
+
+#define  PN532_IRQ      (2)
+#define  INTERRUPT      (1)
+#define  POLLING        (0)
+//use this line for a breakout or shield with an I2C connection
+//check the card by polling
+DFRobot_PN532_IIC  nfc(PN532_IRQ, POLLING);
+DFRobot_PN532:: sCard_t NFCcard;
+
+void setup() {
+  Serial.begin(115200);
+  while (!nfc.begin()) {
+    //Wait for NFC module activation
+    // Serial.println("initial failure");
+    delay (1000);
+  }
+  // Serial.println("Waiting for a card......");
+}
